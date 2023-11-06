28043c5a1b95be4c11f4db105385b177aec601cc
Santiago Linares Avella <s.linaresa@uniandes.edu.co>
2023-11-06 13:12:22 -0500

    Sepracion threads

---
+++ SingleThreadedMiner.java
@@ -0,0 +1,66 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class SingleThreadedMiner extends Thread {
+    private String algorithm;
+    private String data;
+    private int leadingZeros;
+    private AtomicBoolean solutionFound;
+
+    public SingleThreadedMiner(String algorithm, String data, int leadingZeros, AtomicBoolean solutionFound) {
+        this.algorithm = algorithm;
+        this.data = data;
+        this.leadingZeros = leadingZeros;
+        this.solutionFound = solutionFound;
+    }
+
+    @Override
+    public void run() {
+        try {
+            MessageDigest md = MessageDigest.getInstance(algorithm);
+            String prefix = "0".repeat(leadingZeros);
+
+            for (int i = 1; i <= 7; i++) {
+                if (solutionFound.get()) {
+                    return; // Solución encontrada por otro hilo
+                }
+
+                generateHashes(md, prefix, "", i);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void generateHashes(MessageDigest md, String prefix, String current, int length) {
+        if (solutionFound.get()) {
+            return;
+        }
+
+        if (length == 0) {
+            for (char c = 'a'; c <= 'z'; c++) {
+                String valueToHash = current + c;
+                byte[] hashBytes = md.digest(valueToHash.getBytes());
+                String hash = bytesToHex(hashBytes);
+
+                if (hash.startsWith(prefix)) {
+                    solutionFound.set(true);
+                    System.out.println("Solución encontrada: " + valueToHash + " -> " + hash);
+                }
+            }
+        } else {
+            for (char c = 'a'; c <= 'z'; c++) {
+                generateHashes(md, prefix, current + c, length - 1);
+            }
+        }
+    }
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte b : bytes) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
+}
--- caso3/HashMining.java
+++ caso3/HashMining.java
@@ -1,10 +1,8 @@
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class HashMining {
-    private static Thread thread2;
+    private static long startTime;
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
@@ -26,105 +24,38 @@ public class HashMining {
             AtomicBoolean solutionFound = new AtomicBoolean(false);
 
             System.out.println("Comenzando la minería de hash...");
-            MineHash(algorithm, data, leadingZeros, numThreads, solutionFound);
-        }
-    }
 
-    public static void MineHash(String algorithm, String data, int leadingZeros, int numThreads, AtomicBoolean solutionFound) {
-        long startTime = System.currentTimeMillis();
-
-        MineHashTask task1 = new MineHashTask(algorithm, data, leadingZeros, 1, solutionFound);
-        Thread thread1 = new Thread(task1);
-        thread1.start();
-
-        if (numThreads == 2) {
-            MineHashTask task2 = new MineHashTask(algorithm, data, leadingZeros, 2, solutionFound);
-            thread2 = new Thread(task2);
-            thread2.start();
-        }
-
-        try {
-            thread1.join();
-            if (numThreads == 2) {
-                thread2.join();
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        long endTime = System.currentTimeMillis();
-        long elapsedTime = endTime - startTime;
-
-        if (!solutionFound.get()) {
-            System.out.println("No se encontró una solución en todo el espacio de búsqueda.");
-        } else {
-            System.out.println("Tiempo transcurrido: " + elapsedTime + " ms");
-        }
-    }
-}
-
-class MineHashTask implements Runnable {
-    private String algorithm;
-    private String data;
-    private int leadingZeros;
-    private int threadId;
-    private AtomicBoolean solutionFound;
-
-    public MineHashTask(String algorithm, String data, int leadingZeros, int threadId, AtomicBoolean solutionFound) {
-        this.algorithm = algorithm;
-        this.data = data;
-        this.leadingZeros = leadingZeros;
-        this.threadId = threadId;
-        this.solutionFound = solutionFound;
-    }
-
-    @Override
-    public void run() {
-        try {
-            MessageDigest md = MessageDigest.getInstance(algorithm);
-            String prefix = "0".repeat(leadingZeros);
-
-            for (int i = 1; i <= 7; i++) {
-                if (solutionFound.get()) {
-                    return; // Solución encontrada por otro hilo
+            if (numThreads == 1) {
+                SingleThreadedMiner miner = new SingleThreadedMiner(algorithm, data, leadingZeros, solutionFound);
+                miner.start();
+                try {
+                    miner.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+            } else if (numThreads == 2) {
+                MultiThreadedMiner miner1 = new MultiThreadedMiner(algorithm, data, leadingZeros, solutionFound);
+                MultiThreadedMiner miner2 = new MultiThreadedMiner(algorithm, data, leadingZeros, solutionFound);
 
-                generateHashes(md, prefix, "", i);
+                miner1.start();
+                miner2.start();
+
+                try {
+                    miner1.join();
+                    miner2.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-    }
 
-    private void generateHashes(MessageDigest md, String prefix, String current, int length) {
-    if (solutionFound.get()) {
-        return;
-    }
+            long endTime = System.currentTimeMillis();
+            long elapsedTime = endTime - startTime;
 
-    if (length == 0) {
-        for (char c = 'a'; c <= 'z'; c++) {
-            String valueToHash = current + c;
-            byte[] hashBytes = md.digest(valueToHash.getBytes());
-            String hash = bytesToHex(hashBytes);
-
-            if (hash.startsWith(prefix)) {
-                solutionFound.set(true);
-                System.out.println("Thread " + threadId + " encontró una solución: " + valueToHash + " -> " + hash);
+            if (!solutionFound.get()) {
+                System.out.println("No se encontró una solución en todo el espacio de búsqueda.");
+            } else {
+                System.out.println("Tiempo transcurrido: " + elapsedTime + " ms");
             }
         }
-    } else {
-        for (char c = 'a'; c <= 'z'; c++) {
-            generateHashes(md, prefix, current + c, length - 1);
-        }
-    }
-}
-
-
-    private String bytesToHex(byte[] bytes) {
-        StringBuilder result = new StringBuilder();
-        for (byte b : bytes) {
-            result.append(String.format("%02x", b));
-        }
-        return result.toString();
     }
 }
